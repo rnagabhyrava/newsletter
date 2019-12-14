b7e0793207ab3f7eb27564a221a0a2f33e9e65ed
rohithn <rohithnagabhyrava@gmail.com>
2019-12-14 17:08:07 -0500

    Added function comments

--- weather/management/commands/sendmail.py
+++ weather/management/commands/sendmail.py
@@ -8,12 +8,33 @@ class Command(BaseCommand):
     email_dict = {}
     
     def handle(self,*args,**options):
+        """Handle for sending emails
+           Can be called from the terminal via python manage.py sendmail
+
+           Gets all the email objects from database and calls get_and_send_email on each object.
+
+        """
         emails = Email.objects.all()
         for email in emails:
             self.get_and_send_email(email)
     
 
     def get_and_send_email(self,email,email_dict=email_dict):
+        """
+        searches the email_dict for subject and body for the location. 
+        If not available creates the subject and body and stores it for later use.
+
+        params:
+        -------
+        email: email object
+        email_dict: A dictionary that stores the subject and body for each location
+
+
+        returns:
+        --------
+        calls send_email
+
+        """
         location = email.location
         loc_str = location.city + ',' + location.state + ',' + location.country
         if loc_str not in email_dict:
--- weather/utils/weatherEmailHelper.py
+++ weather/utils/weatherEmailHelper.py
@@ -5,6 +5,20 @@ from weather.utils.keys import WEATHER_KEY, MAIL_KEY, FROM_ADDRESS
 
 
 def get_temps(loc_str,key=WEATHER_KEY):
+    """ 
+        params
+        ------
+        loc_str: location for which we need the outputs
+                 eg.format loc_str = "Boston,MA,US"
+
+        key:     openweathermap api key
+
+
+        returns
+        -------
+        current weather status, current temperature
+        and difference of current temperature and tomorrows temperature(24 hrs from now)
+    """
     owm = pyowm.OWM(key)
     now = owm.weather_at_place(loc_str)
     current_temp = now.get_weather().get_temperature('fahrenheit')['temp']
@@ -17,12 +31,29 @@ def get_temps(loc_str,key=WEATHER_KEY):
 
 
 def send_email(email_address,subject,body,mail_api=MAIL_KEY,from_address=FROM_ADDRESS):
+    """
+        sends email via sendgrid uses smtp gmail
+
+        params
+        -------
+        email_address: Recipients email address
+        subject: Subject of the email we wanna send
+        body: Body of the email can be a string or a html file
+        mail_api: sendgrid api key
+        from_address: from_address that we authorised in settings. 
+
+        returns
+        --------
+        prints Email if email is sussefully send
+        else prints the error code/ exception message
+    """
+
     message = Mail(from_email = from_address, to_emails = email_address,
                     subject = subject,html_content = body)
     try:
         sg = SendGridAPIClient(mail_api)
         response = sg.send(message)
-        print(response.status_code)
+        print("Email Sent" if response.status_code==202 else response.status_code)
     except Exception as e:
         print("Sending email for %s failed reason:" % email_address )
         print(e.message)
